eaf1bc50fa62a1ce8dc5a3feb6780d5755712f43
musaksu <musaksu@gmail.com>
2023-01-03 23:52:46 +0100

    Task 5: Assign Variables

---
+++ src/day3_variables_dataTypes/Task5_AssingVariables.java
@@ -0,0 +1,13 @@
+package day3_variables_dataTypes;
+
+public class Task5_AssingVariables {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 54;
+        int c = 67;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
