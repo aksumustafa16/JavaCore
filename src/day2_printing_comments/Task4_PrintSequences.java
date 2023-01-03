fa1c669d1a688c504c91b8868c8f0c78a4456638
musaksu <musaksu@gmail.com>
2023-01-03 22:42:12 +0100

    Print Squences

---
+++ src/day2_printing_comments/Task4_PrintSequences.java
@@ -0,0 +1,10 @@
+package day2_printing_comments;
+
+public class Task4_PrintSequences {
+    public static void main(String[] args) {
+        System.out.println("\"\\__/\"\n");
+        System.out.println("He said \"Hello\".\n");
+        System.out.println("Would you like \'coffee\' or \'tea\'");
+
+    }
+}
