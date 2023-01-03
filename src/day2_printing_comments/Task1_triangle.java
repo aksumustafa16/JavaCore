cc52e19dc3f64c3c314d79bbb9337b55ee274fac
musaksu <musaksu@gmail.com>
2023-01-03 22:26:16 +0100

    Change Name

--- src/day2_printing_comments/Task1_triangle.java
+++ src/day2_printing_comments/Task1_triangle.java
@@ -1,6 +1,6 @@
 package day2_printing_comments;
 
-public class triangle {
+public class Task1_triangle {
     public static void main(String[] args) {
         System.out.println("    *");
         System.out.println("   * *");
