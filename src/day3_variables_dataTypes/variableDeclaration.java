2727b51de54b73f4db00caa1b96b1190bb348b6f
musaksu <musaksu@gmail.com>
2023-01-03 23:04:00 +0100

    Variable Decleration

---
+++ src/day3_variables_dataTypes/variableDeclaration.java
@@ -0,0 +1,34 @@
+package day3_variables_dataTypes;
+
+public class variableDeclaration {
+    public static void main(String[] args) {
+        //declaration
+        byte inches;
+        int speed;
+        short month;
+        float salesCommision;
+        double distance;
+
+        //assigned
+        inches=5;
+        speed = 200;
+        month = 2;
+        distance = 40.2;
+        salesCommision = 5;
+
+        //declared+assigned
+        byte feet = 5;
+
+        System.out.println(inches);
+        System.out.println(speed);
+
+        int accountnumber = 10;
+
+        System.out.println("My number is " + accountnumber);
+        System.out.println("My number is 15");
+        System.out.println("My number is 20");
+
+        //Test case - > Check the balances with 100 different account numbers
+
+    }
+}
