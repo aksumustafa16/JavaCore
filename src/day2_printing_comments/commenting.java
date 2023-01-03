ae70b5c066e46102d5c431bb17469246414ee7bb
musaksu <musaksu@gmail.com>
2023-01-03 22:47:22 +0100

    Commenting

---
+++ src/day2_printing_comments/commenting.java
@@ -0,0 +1,21 @@
+package day2_printing_comments;
+
+public class commenting {
+    public static void main(String[] args) {
+        //Author: Mustafa
+        //Time: 4:30PM
+
+        System.out.println("Hello World");
+
+        /*Today class was about print statements,
+        and we did several tasks for practice.*/
+
+        /*
+         *
+         */
+
+        /**
+         *
+         */
+    }
+}
