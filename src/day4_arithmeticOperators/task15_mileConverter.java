480a7760bab080330efeaf6191b29a4fb71242f2
musaksu <musaksu@gmail.com>
2023-01-04 22:44:02 +0100

    Mile Converter

---
+++ src/day4_arithmeticOperators/task15_mileConverter.java
@@ -0,0 +1,12 @@
+package day4_arithmeticOperators;
+
+public class task15_mileConverter {
+    public static void main(String[] args) {
+        double mile,km;
+
+        mile = 85;
+
+        km = mile * 1.609344;
+        System.out.println(mile  + " mile is equal to " + km + "km");
+    }
+}
