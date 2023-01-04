adbc4cd3ccb1d6260b9faa2ea3bd81c762d1ec0e
musaksu <musaksu@gmail.com>
2023-01-04 22:34:09 +0100

    Outcome

---
+++ src/day4_arithmeticOperators/task12_outcome.java
@@ -0,0 +1,9 @@
+package day4_arithmeticOperators;
+
+public class task12_outcome {
+    public static void main(String[] args) {
+
+        int outcome = 12+6/3;
+        System.out.println(outcome);
+    }
+}
