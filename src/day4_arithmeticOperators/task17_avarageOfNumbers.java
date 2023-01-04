3673ca0227449fbe0d513d0814de8f1893e7a123
musaksu <musaksu@gmail.com>
2023-01-04 22:50:44 +0100

    Avarage Of Numbers

---
+++ src/day4_arithmeticOperators/task17_avarageOfNumbers.java
@@ -0,0 +1,11 @@
+package day4_arithmeticOperators;
+
+public class task17_avarageOfNumbers {
+    public static void main(String[] args) {
+        int num1 = 46;
+        int num2 = 56;
+        int num3 = 74;
+        int avg = (num1 + num2 + num3)/3;
+        System.out.println("avg = " + avg);
+    }
+}
