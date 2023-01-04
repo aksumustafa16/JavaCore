7ee6c823bbcb200f1b69aa75457ed7ff8dcb6dd5
musaksu <musaksu@gmail.com>
2023-01-04 22:36:07 +0100

    FindSum

---
+++ src/day4_arithmeticOperators/findSum.java
@@ -0,0 +1,12 @@
+package day4_arithmeticOperators;
+
+public class findSum {
+    public static void main(String[] args) {
+        int num1 = 34;
+        int num2 = 63;
+        int sum = num1 + num2;
+
+        System.out.println("sum = " + sum);
+
+    }
+}
