27d3c464d5e23763d30475101d8167ae2b33ced9
musaksu <musaksu@gmail.com>
2023-01-04 21:17:50 +0100

    Changed Year Program

---
+++ src/day3_variables_dataTypes/task8_ChangedYearProgram.java
@@ -0,0 +1,9 @@
+package day3_variables_dataTypes;
+
+public class task8_ChangedYearProgram {
+    public static void main(String[] args) {
+        int year = 1492;
+
+        System.out.println("IN " + year + " COLUMBUS SAILED THE OCEAN BLUE.");
+    }
+}
