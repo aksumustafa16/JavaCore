dcf8a59cddecd2028d96bbb20b5af567bc6b1071
musaksu <musaksu@gmail.com>
2023-01-06 23:46:39 +0100

    Assigment Operators

---
+++ src/day5_unary_assigment_relational_Operators/assigmentOperators.java
@@ -0,0 +1,24 @@
+package day5_unary_assigment_relational_Operators;
+
+public class assigmentOperators {
+    public static void main(String[] args) {
+
+        int i = 100;
+        System.out.println("i = " + i);
+
+        i+=1;
+        System.out.println("i = " + i);
+
+        i-=2;
+        System.out.println("i = " + i);
+
+        i*=3;
+        System.out.println("i = " + i);
+
+        i/=4;
+        System.out.println("i = " + i);
+
+        i%=5;
+        System.out.println("i = " + i);
+    }
+}
