eb27612d6dd990a21da151c884ec522d1ab19a6c
musaksu <musaksu@gmail.com>
2023-01-07 23:08:06 +0100

    Operator Precedence

--- src/day6_logicalOperators/operatorsPrecedence.java
+++ src/day6_logicalOperators/operatorsPrecedence.java
@@ -9,5 +9,29 @@ public class operatorsPrecedence {
 
         boolean comp = applesCount<orangesCount||orangesCount>=pearsCount;
         System.out.println("comp = " + comp);
+
+        System.out.println("*******************************");
+
+        String outsideWeather;
+        int degree;
+        outsideWeather = "Shinny";
+        degree = 70;
+
+        boolean comp2 = (!(outsideWeather=="Rainy"||degree==70));
+        System.out.println("comp2 = " + comp2);
+
+        System.out.println("*****************************");
+
+
+        int b = 2;
+        boolean res = ++b ==2 || --b == 2 && --b == 2;
+        System.out.println("res = " + res);
+
+        System.out.println("*******************************");
+
+        boolean x = true, z = true;
+        int y = 20;
+        x = (y!=10)||(z=false);
+        System.out.println("x = " + x);
     }
 }
