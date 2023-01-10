bbe8775e76c6dee1865e911276b304b6168f72ee
musaksu <musaksu@gmail.com>
2023-01-10 23:28:03 +0100

    Nested If Statement

---
+++ src/day8_controlFlowStatement2/nestedIfStatement.java
@@ -0,0 +1,25 @@
+package day8_controlFlowStatement2;
+
+public class nestedIfStatement {
+    public static void main(String[] args) {
+
+        boolean isRushHour = true;
+        int carType = 1;
+        double price = 0.0;
+
+        if (carType == 1) {
+            if (isRushHour) {
+                price = 30.0;
+            }else{
+                price = 5.0;
+            }
+        }else if(carType==2){
+            if (isRushHour) {
+                price = 55.30;
+            }else{
+                price = 15.99;
+            }
+        }
+        System.out.println("Total cost: " + price );
+    }
+}
