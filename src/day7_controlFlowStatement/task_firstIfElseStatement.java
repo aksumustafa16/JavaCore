ac85526e9eee24429052ef4b92d882749d25afe5
musaksu <musaksu@gmail.com>
2023-01-10 23:00:40 +0100

    task_firstIfElseStatement

---
+++ src/day7_controlFlowStatement/task_firstIfElseStatement.java
@@ -0,0 +1,15 @@
+package day7_controlFlowStatement;
+
+public class task_firstIfElseStatement {
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y=0;
+        if (x > 100) {
+            y=20;
+        }else{
+            y=0;
+        }
+        System.out.println(y);
+    }
+}
