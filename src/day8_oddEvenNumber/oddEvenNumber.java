75f9be3f6e46f66791c13a9d67b36dea3137b1d8
musaksu <musaksu@gmail.com>
2023-01-10 23:06:16 +0100

    oddEvenNumber

---
+++ src/day8_oddEvenNumber/oddEvenNumber.java
@@ -0,0 +1,15 @@
+package day8_oddEvenNumber;
+
+public class oddEvenNumber {
+    public static void main(String[] args) {
+
+        int num1 = 21;
+
+        if (num1%2 == 0) {
+            System.out.println("num is a even number");
+        }else{
+            System.out.println("num is a odd number");
+        }
+
+    }
+}
