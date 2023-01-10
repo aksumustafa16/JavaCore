507a4b137699cfb5181f5df90967be3a0499f4ed
musaksu <musaksu@gmail.com>
2023-01-10 23:39:12 +0100

    Switch Case Statement

---
+++ src/day8_controlFlowStatement2/SwitchCaseStatement.java
@@ -0,0 +1,25 @@
+package day8_controlFlowStatement2;
+
+public class SwitchCaseStatement {
+    public static void main(String[] args) {
+
+        String weather = "sunny";
+
+        switch (weather) {
+            case "sunny":
+                System.out.println("Go to park");
+                break;
+            case "hot":
+                System.out.println("Go to swimming");
+                break;
+            case "windy":
+                System.out.println("Fly a kite");
+                break;
+            case "snwo":
+                System.out.println("Go snowboarding");
+                break;
+            default:
+                System.out.println("Code Java in any other weather");
+        }
+    }
+}
--- src/day8_controlFlowStatement2/task_largestNumber.java
+++ src/day8_controlFlowStatement2/task_largestNumber.java
@@ -9,12 +9,10 @@ public class task_largestNumber {
 
         if (num1 > num2 && num1 > num3) {
             System.out.println("Num1 is greatest one");
-        } else if (num2 > num1 && num2 > num3) {
+        } else if (num2>num3) {
             System.out.println("Num2 is greatest one");
-        } else if (num3 > num1 && num3 > num2) {
+        } else {
             System.out.println("Num3 is greatest one");
-        }else{
-            System.out.println("Numbers is equal");
         }
     }
 }
