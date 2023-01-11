82e0203d1d586c6d264c4c5f20fc57718724fb8c
musaksu <musaksu@gmail.com>
2023-01-11 21:38:49 +0100

    Plus Table

---
+++ src/day10_controlFlowStatements_Part4/task_PlusTable.java
@@ -0,0 +1,10 @@
+package day10_controlFlowStatements_Part4;
+
+public class task_PlusTable {
+    public static void main(String[] args) {
+
+        for (int i=1;i<=10;i++){
+            System.out.println(i + "x" + i  + "= " + i*i);
+        }
+    }
+}
