c3a63aa41a8a036451cd785864d12289596152dd
musaksu <musaksu@gmail.com>
2023-01-11 09:36:11 +0100

    Traffic Light Selector

---
+++ src/day8_controlFlowStatement2/TrafficLightSelector.java
@@ -0,0 +1,22 @@
+package day8_controlFlowStatement2;
+
+public class TrafficLightSelector {
+    public static void main(String[] args) {
+
+        char color = 'r';
+
+        switch (color){
+            case 'r': case 'R':
+                System.out.println("Red Light");
+                break;
+            case 'o': case 'O':
+                System.out.println("Orange Light");
+                break;
+            case 'g': case 'G':
+                System.out.println("Green Light");
+                break;
+            default:
+                System.out.println("Wrong Selector");
+        }
+    }
+}
