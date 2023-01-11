c89043b0c85a90969f9fa380bb25314190c0bf7b
musaksu <musaksu@gmail.com>
2023-01-11 21:20:13 +0100

    Show Even Number

---
+++ src/day10_controlFlowStatements_Part4/task_ShowEvenNumbers.java
@@ -0,0 +1,15 @@
+package day10_controlFlowStatements_Part4;
+
+public class task_ShowEvenNumbers {
+    public static void main(String[] args) {
+
+
+        for (int i=0;i<=100;i++){
+
+            if (i % 2 == 0) {
+
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
